c5d7166ff63742d8c8b16de887a4f07ddc5498d6
Valentina Fuentes <fuentesvalentina74@gmail.com>
2023-10-31 21:53:15 -0300

    Corrijo un espacio en el ejercicio 6

--- js/06-ejercicio.js
+++ js/06-ejercicio.js
@@ -4,5 +4,5 @@ let numero2 = parseInt(prompt('Ingrese otro número'));
 if(numero1>numero2){
     document.write('El número mayor es'+(numero1));
 }else{
-    document.write('Elnúmero mayor es el'+(numero2));
+    document.write('El número mayor es el'+(numero2));
 }
