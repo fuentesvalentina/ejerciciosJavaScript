64450679e38c10da4015b76127454d43e631e7ac
Valentina Fuentes <fuentesvalentina74@gmail.com>
2023-10-31 21:35:32 -0300

    Agregamos ejercicio numero 5

---
+++ js/05-ejercicio.js
@@ -0,0 +1,3 @@
+let numero1 = parseInt(prompt('Ingrese un número'));
+let numero2 = parseInt(prompt('Ingrese otro numero'));
+document.write('El resultado de la suma es igual a '+(numero1+numero2));
