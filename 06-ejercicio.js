cab7918edf9f9877b1a0809c5d77425b91037812
Valentina Fuentes <fuentesvalentina74@gmail.com>
2023-10-31 21:48:43 -0300

    Agrego ejercicio numero 6

---
+++ 06-ejercicio.js
@@ -0,0 +1,8 @@
+let numero1 = parseInt(prompt('Ingrese un número'));
+let numero2 = parseInt(prompt('Ingrese otro número'));
+
+if(numero1>numero2){
+    document.write('El número mayor es'+(numero1));
+}else{
+    document.write('Elnúmero mayor es el'+(numero2));
+}
