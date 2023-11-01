722d3e685826b7e33dc996254fb4df1c0302b82c
Valentina Fuentes <fuentesvalentina74@gmail.com>
2023-11-01 14:38:37 -0300

    Corrijo ejercicio numero 10

--- js/10-ejercicio.js
+++ js/10-ejercicio.js
@@ -1,25 +1,16 @@
-// Solicitar al usuario que ingrese un número
-let numero = parseInt(prompt("Ingrese un número:"));
+// Pedir al usuario un número
+const numero = parseInt(prompt("Ingrese un número:"));
 
-// Verificar la divisibilidad por 2, 3, 5 y 7
-if (numero % 2 === 0) {
-   document.write(numero + " es divisible por 2.");
-} 
-document.write('<br>');
-if (numero % 3 === 0) {
-    document.write(numero + " es divisible por 3.");
-} 
-document.write('<br>');
-if (numero % 5 === 0) {
-    document.write(numero + " es divisible por 5.");
-} 
-document.write('<br>');
-if (numero % 7 === 0) {
-    document.write(numero + " es divisible por 7.");
-} 
-document.write('<br>');
+let esDivisible = false;
 
-// Si no es divisible por ninguno de los números mencionados
-if (numero % 2 !== 0 && numero % 3 !== 0 && numero % 5 !== 0 && numero % 7 !== 0) {
-    document.write(numero + " no es divisible por 2, 3, 5 ni 7.");
+// Comprobar si el número es divisible por 2, 3, 5 o 7
+if (numero % 2 === 0 || numero % 3 === 0 || numero % 5 === 0 || numero % 7 === 0) {
+  esDivisible = true;
+}
+
+// Mostrar el resultado
+if (esDivisible) {
+  document.write('es divisible por 2, 3, 5 o 7.');
+} else {
+  document.write('no es divisible por 2, 3, 5 ni 7.');
 }
